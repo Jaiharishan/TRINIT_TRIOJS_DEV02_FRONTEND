cc82dc271e6133616b8da7a935d623ec025f89e4
Santhosh <santhoshm12052003@gmail.com>
2022-01-30 08:52:20 +0530

    comment a bug - comment a comment

--- components/DiscussionCard.tsx
+++ components/DiscussionCard.tsx
@@ -3,21 +3,40 @@ import { AddThreadModal } from ".";
 const DiscussionCard = ({elem}) => {
   return (
     <>
-      <div className="flex rounded-xl flex-col border-2 border-gray-600 lg:w-8/12">
-      <div className="px-2 py-4 border-b-2 border-gray-600 text-white text-lg flex items-center gap-2">
-        <div className="w-6 h-6 rounded-full bg-blue-600">
-          <img src={`http://localhost:4000/${elem.openedBy.profilePic}`} alt="" />
+    <div className='flex align-center'>
+        <div className="flex rounded-xl flex-col border-2 border-gray-600 lg:w-8/12">
+        <div className="px-2 py-4 border-b-2 border-gray-600 text-white text-lg flex items-center gap-2">
+          <div className="w-6 h-6 rounded-full bg-blue-600">
+            <img src={`http://localhost:4000/${elem.openedBy.profilePic}`} alt="" />
+          </div>
+          {elem.openedBy.userName}
         </div>
-        {elem.openedBy.userName}
+        <div className="text-md text-white py-4 px-2">
+        {elem.description}
+        </div>
+        </div>
+  
+        <div className=" m-5">
+          <AddThreadModal commentId={elem._id} />
+        </div>
       </div>
-      <div className="text-md text-white py-4 px-2">
-      {elem.description}
+      <div>
+      {elem?.comments?.map((com)=>(
+          <div className="flex rounded-xl flex-col border-2 border-gray-600 lg:w-8/12 ml-8 m-5">
+          <div className="px-2 py-4 border-b-2 border-gray-600 text-white text-lg flex items-center gap-2">
+            <div className="w-6 h-6 rounded-full bg-blue-600">
+              <img src={`http://localhost:4000/${com.openedBy.profilePic}`} alt="" />
+            </div>
+            {com.openedBy.userName}
+          </div>
+          <div className="text-md text-white py-4 px-2">
+          {com.description}
+          </div>
+          </div>
+    
+            ))}
       </div>
-      </div>
-
-      <div className="mt-2">
-        <AddThreadModal />
-      </div>
+      
     </>
   );
 };
